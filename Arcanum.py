e38cf9f1aebfa049eaa7db35e4a6da0c54257c66
Leon Vaiman <leon@freed.tv>
2014-07-24 09:17:01 +0300

    minor.

--- Arcanum.py
+++ Arcanum.py
@@ -5,7 +5,7 @@ from src.Arcanum import Arcanum
 if __name__ == "__main__":
 
     srcPath = path.dirname(sys.argv[0])
-    dataPath = path.join(srcPath, "GameData")
+    dataPath = path.join(srcPath, r"GameData\WipArcanum")
 
     arcanum = Arcanum(dataPath)
     arcanum.Run()
